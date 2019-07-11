3b0fd191005852c74afef874401e945f6535496e
Kelly Meier <kemeier2000@yahoo.com>
2019-07-11 17:06:06 -0500

    additional js work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,3 +3,28 @@ var wins=0;
 var losses=0;
 var guessesLeft=10;
 
+var directionsText=document.getElementById("directions-text");
+var winsText=document.getElementById("wins-text");
+var lossesText=document.getElementById("losses-text");
+var guessesLeft=document.getElementById("guessesleft-text");
+var guessesSoFar=document.getElementById("guessessofar-text");
+
+document.onkeyup=function(event) {  
+var userGuess=event.key;    
+
+var computerGuess=computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+if (userGuess===computerGuess) {
+    wins++;
+} else {
+    losses++;
+}
+
+guessesSoFar.textContent="Your guesses so far: "+userGuess;
+guessesLeft.textContent="Guesses left: "+guessesLeft;
+wins.textContent="Wins: "+wins;
+losses.textContent="Losses: "+losses;
+};
+
+
+
